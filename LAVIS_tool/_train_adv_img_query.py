7a5644cc7c3e863788c903b90c90692719b7b6bd
khoa16122004 <khoatr.work@gmail.com>
2025-03-06 16:37:31 +0700

    Update _train_adv_img_query.py

--- LAVIS_tool/_train_adv_img_query.py
+++ LAVIS_tool/_train_adv_img_query.py
@@ -449,6 +449,8 @@ if __name__ == "__main__":
 
         torchvision.utils.save_image(adv_image_in_current_step / 255.0, os.path.join(args.output, basename))
         torchvision.utils.save_image(image_clean / 255.0, os.path.join(args.output, "clean_" + basename))
+        torchvision.utils.save_image(delta_data / 255.0, os.path.join(args.output, "pertu_" + basename))
+        
         print("best caption of current image:", lavis_text_of_adv_image_in_current_step)
         with open(os.path.join(args.output + '.txt'), 'a') as f:
             # print(''.join([best_caption]), file=f)
