057c4111730dac7a4c962e96b98a33e5849b9dbd
khoa16122004 <khoatr.work@gmail.com>
2025-03-06 15:34:02 +0700

    Update _train_adv_img_query.py

--- LAVIS_tool/_train_adv_img_query.py
+++ LAVIS_tool/_train_adv_img_query.py
@@ -133,7 +133,6 @@ def _i2t(args, txt_processors, model, image):
     else:
         samples  = {"image": image}
         print(samples)
-
         caption  = model.generate(samples, use_nucleus_sampling=True, num_captions=1)
         caption_merged = caption
     
@@ -339,8 +338,6 @@ if __name__ == "__main__":
                     text_of_sub_perturbed_imgs = _i2t(args, txt_processors, model, image=sub_perturbed_image_repeat)
                 else:
                     with torch.no_grad():
-                        print()
-
                         text_of_sub_perturbed_imgs = _i2t(args, txt_processors, model, image=sub_perturbed_image_repeat)
                 text_of_perturbed_imgs.extend(text_of_sub_perturbed_imgs)
             
