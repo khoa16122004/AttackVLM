bd7c81e3eea63a5b46847cd670185fc2682f8ad9
khoa16122004 <khoatr.work@gmail.com>
2025-03-06 17:11:11 +0700

    Update run.sh

--- LAVIS_tool/run.sh
+++ LAVIS_tool/run.sh
@@ -1,2 +1,6 @@
-python _train_adv_img_query.py --batch_size 10 --num_samples 1 --image_dir images --target_dir images
-python _train_adv_img_query.py --input_res 224 --epsilon 2 --steps 100  --output test_query --data_path images --text_path target_annotations.txt --num_query 100 --image_dir images --target_dir target_image/samples --annotation_file annotations.txt
+#!/bin/sh
+#SBATCH --gres=gpu:1 # So GPU can dung
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+python LAVIS_tool/_train_adv_img_query.py --input_res 224 --epsilon 2 --steps 50  --output test_query --data_path images --text_path target_annotations.txt --num_query 500 --image_dir images --target_dir target_image/samples --annotation_file annotations.txt --num_samples 5
