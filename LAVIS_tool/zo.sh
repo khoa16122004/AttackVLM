626e3c62f5f0669b2a81f1faa72aee3ea5570834
khoa16122004 <khoatr.work@gmail.com>
2025-03-09 16:03:09 +0700

    Update zo.sh

--- LAVIS_tool/zo.sh
+++ LAVIS_tool/zo.sh
@@ -1,1 +1,6 @@
+#!/bin/sh
+#SBATCH --gres=gpu:1 # So GPU can dung
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 python LAVIS_tool/zo.py --steps 8 --epsilon 0.03 --num_query 100 --output_dir zo_ii --image_dir images --target_dir target_image/samples --annotation_path annotations.txt 
