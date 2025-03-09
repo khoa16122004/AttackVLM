26b967a76327dab0c582138538202195ab259b0a
khoa16122004 <khoatr.work@gmail.com>
2025-03-09 16:46:53 +0700

    Update zo.sh

--- LAVIS_tool/zo.sh
+++ LAVIS_tool/zo.sh
@@ -3,4 +3,5 @@
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-python LAVIS_tool/zo.py --steps 8 --epsilon 0.03 --num_query 100 --output_dir zo_ii --image_dir images --target_dir target_image/samples --annotation_path annotations.txt 
+
+python LAVIS_tool/zo.py --steps 8 --epsilon 0.03 --num_query 100 --output_dir zo_ii --image_dir images --target_dir target_image/samples --annotation_path annotations.txt --num_samples 1000
