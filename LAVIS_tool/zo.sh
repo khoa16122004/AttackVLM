671e163be6a990dfd0977b333d8efa9a4415bdfe
khoa16122004 <khoatr.work@gmail.com>
2025-03-10 16:58:13 +0700

    Update zo.sh

--- LAVIS_tool/zo.sh
+++ LAVIS_tool/zo.sh
@@ -4,4 +4,4 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 
-python LAVIS_tool/zo.py --steps 8 --epsilon 0.03 --num_query 100 --output_dir zo_ii --image_dir images --target_dir target_image/samples --annotation_path annotations.txt --num_samples 1000
+python LAVIS_tool/transfer_and_query.py --steps 100 --alpha 1 --epsilon 0.03 --sigma 0.01 --output_dir zo_MF_ii_tt --image_dir transfer_ii_1000_100_1.0_0.03_0.01 --target_dir images --annotation_path annotations.txt --num_samples 1000 --method zo_MF_tt --model_name blip2_opt --model_type pretrain_opt2.7b
