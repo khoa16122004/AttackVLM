7e1dabe446f753fa32e53ad028364f223fbd323c
khoa16122004 <khoatr.work@gmail.com>
2025-03-11 13:08:51 +0700

    Update evaluate.py

--- LAVIS_tool/evaluate.py
+++ LAVIS_tool/evaluate.py
@@ -40,7 +40,6 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output_dir", default="zo", type=str)
     parser.add_argument("--result_path", type=str)
     args = parser.parse_args()
     
